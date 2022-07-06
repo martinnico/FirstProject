1c0cb94d7a30921771cfae61f83b7c2498a0e1dc
Martin Puebla <martin.nico37@gmail.com>
2022-07-06 13:10:19 -0300

    Se modifico el endpoint updateTechnology y deleteTechnology
    Se cambio la logica de negocio utilizando builders y logs

--- src/main/java/com/demo/firstproject/services/impl/TechnologyServiceImp.java
+++ src/main/java/com/demo/firstproject/services/impl/TechnologyServiceImp.java
@@ -4,16 +4,17 @@ package com.demo.firstproject.services.impl;
 import java.util.List;
 import java.util.Optional;
 
-import com.demo.firstproject.exception.TechnologyNotFound;
+import com.demo.firstproject.exception.TechnologyNotFoundException;
 import com.demo.firstproject.models.dto.TechnologyDto;
 import com.demo.firstproject.repository.TechnologyRepository;
 import com.demo.firstproject.services.TechnologyService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.demo.firstproject.models.TechnologyModel;
 
 
-
+@Slf4j
 @Service
 public class TechnologyServiceImp implements TechnologyService {
 
@@ -25,40 +26,45 @@ public class TechnologyServiceImp implements TechnologyService {
         return  technologyRepository.findAll();
     }
 
-    public TechnologyModel createTechnology(TechnologyDto technologyDto)
+    public void createTechnology(TechnologyDto technologyDto)
     {
-        return technologyRepository.save(TechnologyModel.builder()
+         technologyRepository.save(TechnologyModel.builder()
                         .nameTechnology(technologyDto.getNameTechnology())
                         .version(technologyDto.getVersion())
                         .build());
+         log.debug("Se ha agregado la tecnologia: "+technologyDto);
     }
 
-    public TechnologyModel updateTechnology(Long id, TechnologyDto technologyDto){
-        Optional<TechnologyModel> result = technologyRepository.findById(id);
-        if(result.isPresent()){
-            result.get().setNameTechnology(technologyDto.getNameTechnology());
-            result.get().setVersion(technologyDto.getVersion());
-            return technologyRepository.save(result.get());
+    public void updateTechnology(Long id, TechnologyDto technologyDto) {
+        TechnologyModel result = technologyRepository.findById(id).orElseThrow(() -> new TechnologyNotFoundException("Tecnologia no encontrada "));
+        try {
+        if (result != null) {
+            result = TechnologyModel.builder()
+                    .nameTechnology(technologyDto.getNameTechnology())
+                    .version(technologyDto.getVersion())
+                    .build();
+            technologyRepository.save(result);
         }
-        else {
-            throw new TechnologyNotFound("El id :"+ id + " no existe");
+        } catch (TechnologyNotFoundException t){
+            log.error("La tecnologia con id: "+id+" no se ha encontrado");
         }
     }
 
     public void deleteTechnology (Long id){
         Optional<TechnologyModel> result = technologyRepository.findById(id);
-        if(result.isPresent()){
-            technologyRepository.delete(result.get());
-        }
-        else {
-            throw new TechnologyNotFound("EL id: "+ id + " no existe");
+        try {
+            if (result.isPresent()) {
+                technologyRepository.delete(result.get());
+            }
+        }catch (TechnologyNotFoundException t){
+            log.error("La tecnologia con el id: "+id+" no se ha encontrado");
         }
     }
 
 
 
     public TechnologyDto findTechnologyDto (Long id){
-        TechnologyModel technologyModel = technologyRepository.getById(id);
+        TechnologyModel technologyModel = technologyRepository.findById(id).orElseThrow(()->new TechnologyNotFoundException("Tecnologia no encontrada "));;
         return  TechnologyDto.builder()
                 .nameTechnology(technologyModel.getNameTechnology())
                 .version(technologyModel.getVersion())
