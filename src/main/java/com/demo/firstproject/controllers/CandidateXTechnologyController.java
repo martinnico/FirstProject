79f1d7fa6f4422b5645868f10220c4fa45d54a6e
Martin Puebla <martin.nico37@gmail.com>
2022-07-07 16:49:25 -0300

    Se agrego la documentacion de Swagger en CandidateXTechnologyController

--- src/main/java/com/demo/firstproject/controllers/CandidateXTechnologyController.java
+++ src/main/java/com/demo/firstproject/controllers/CandidateXTechnologyController.java
@@ -4,6 +4,11 @@ import com.demo.firstproject.models.CandidateXTechnology;
 import com.demo.firstproject.models.dto.CandidateXTechnologyDto;
 import com.demo.firstproject.projections.ListCandidatesProjection;
 import com.demo.firstproject.services.impl.CandidateXTechnologyServiceImp;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@Tag(name = "Experiences")
 @RestController
 @RequestMapping("/candidateXTechnology")
 public class CandidateXTechnologyController {
@@ -24,35 +30,62 @@ public class CandidateXTechnologyController {
         @Autowired
         private CandidateXTechnologyServiceImp service;
 
+        @Operation(summary = "Show All Experiences")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json"))
+        })
         @GetMapping
         public ResponseEntity <List<CandidateXTechnology>> getCandidateXTechnology(){
             return new ResponseEntity<>(service.getCandidateXTechnology(),HttpStatus.OK);
 
         }
+         @Operation(summary = "Create Experiences")
+         @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", content = @Content)
+          })
         @PostMapping
         public ResponseEntity<CandidateXTechnology> createCandidateXTechnology(@RequestBody CandidateXTechnologyDto candidateXTechnologyDtoSend) {
             service.createCandidateXTechnology(candidateXTechnologyDtoSend);
             return new ResponseEntity<>(HttpStatus.CREATED);
         }
 
+        @Operation(summary = "Update Experience by Id")
+        @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Successfully updated Experience", content = @Content),
+        @ApiResponse(responseCode = "404", description = "Experience ID not found", content = @Content)
+            })
         @PutMapping("/{id}")
         public ResponseEntity<CandidateXTechnology> updateCandidateXTechnology(@PathVariable ("id") Long id, @RequestBody CandidateXTechnologyDto candidateXTechnologyDtoSend) {
             service.updateCandidateXTechnology(id,candidateXTechnologyDtoSend);
             return new ResponseEntity<>(HttpStatus.OK);
         }
 
+         @Operation(summary = "Delete Experience by Id")
+         @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted Experience", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Experience not found", content = @Content)
+    })
         @DeleteMapping("/{id}")
         public ResponseEntity<Void> deleteCandidateXTechnology(@PathVariable ("id") Long id){
             service.deleteCandidateXTechnology(id);
             return new ResponseEntity<> (HttpStatus.NO_CONTENT) ;
         }
 
+         @Operation(summary = "Get All Experiences by Candidates Id")
+            @ApiResponses(value = {
+                @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json")),
+                @ApiResponse(responseCode = "404", description = "Candidate Id not found", content = @Content)
+    })
         @GetMapping(value = "/dto/{candidateId}")
         public ResponseEntity<CandidateXTechnologyDto> findCandidateXTechnologyDto(@PathVariable Long candidateId){
             service.findCandidateXTechnologyDto(candidateId);
             return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "Get All Experiences")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json"))
+    })
         @GetMapping(value = "/listcandidate")
         public ResponseEntity<List<ListCandidatesProjection>> listCandidateXTechnology(@PathVariable String technology){
             return new ResponseEntity<>(service.listCandidatesXTechnologies(technology),HttpStatus.OK);
