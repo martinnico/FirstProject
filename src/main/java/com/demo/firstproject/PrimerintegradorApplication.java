c9a37cf6991fe0f5e619e2e55f35231f653a872b
Martin Puebla <martin.nico37@gmail.com>
2022-07-07 17:52:18 -0300

    Soluciones

--- src/main/java/com/demo/firstproject/PrimerintegradorApplication.java
+++ src/main/java/com/demo/firstproject/PrimerintegradorApplication.java
@@ -2,13 +2,14 @@ package com.demo.firstproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
-@Configuration
-@EnableSwagger2
+@EnableWebMvc
 public class PrimerintegradorApplication {
     
 
--- src/main/java/com/demo/firstproject/exception/CandidateNotFoundException.java
+++ src/main/java/com/demo/firstproject/exception/CandidateNotFoundException.java
@@ -1,5 +1,9 @@
 package com.demo.firstproject.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CandidateNotFoundException extends RuntimeException{
     public CandidateNotFoundException(String message){
         super (message);
