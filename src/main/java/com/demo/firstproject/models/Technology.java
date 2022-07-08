bbd20765b4d2ffdf03c5b0bcbd4e7720cf4745b7
Martin Puebla <martin.nico37@gmail.com>
2022-07-08 00:29:53 -0300

    Se agregaron las notaciones @NotNull y @NotBlank en la entity Technology

--- src/main/java/com/demo/firstproject/models/Technology.java
+++ src/main/java/com/demo/firstproject/models/Technology.java
@@ -1,12 +1,18 @@
-
 package com.demo.firstproject.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.*;
-import javax.validation.constraints.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 
 @Data
@@ -14,17 +20,20 @@ import javax.validation.constraints.*;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name="Technology")
+@Table(name = "Technology")
 public class Technology {
-    
+
+    @NotNull
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idTechnology;
-    
+
+    @NotBlank
     @NotEmpty(message = "completar con nombre de tecnologia")
     private String nameTechnology;
-    
+
+    @NotBlank
     @NotEmpty(message = "completar con numero de version")
     private String version;
-    
+
 }
