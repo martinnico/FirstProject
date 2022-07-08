c9fc1ecc095cc9479f48bfb1fd4bbd1874212fba
Martin Puebla <martin.nico37@gmail.com>
2022-07-08 00:24:30 -0300

    Se agregaron las notaciones @NotNull y @NotBlank en la entity candidate

--- src/main/java/com/demo/firstproject/models/Candidate.java
+++ src/main/java/com/demo/firstproject/models/Candidate.java
@@ -16,23 +16,29 @@ import javax.validation.constraints.*;
 @Entity
 @Table(name="Candidate")
 public class Candidate {
-    
+
+    @NotNull
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @NotBlank
     @NotEmpty(message = "completar nombre")
     private String name;
-    
+
+    @NotBlank
     @NotEmpty(message = "completar Apellido")
     private String lastName;
-    
+
+    @NotBlank
     @NotEmpty(message = "completar tipo de DNI")
     private TypeOfDni typeDni;
-    
+
+    @NotBlank
     @NotEmpty(message = "completar numero de DNI")
     private long documentNumber;
 
+    @NotBlank
     private boolean active ;
 
     @Temporal(TemporalType.DATE)
