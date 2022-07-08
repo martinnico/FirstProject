3d0cb7641a980d7328586a36f691cadf0c599d0e
Martin Puebla <martin.nico37@gmail.com>
2022-07-08 00:27:44 -0300

    Se agregaron las notaciones @NotNull y @NotBlank con mensajes en la entity CandidateXTechnology

--- src/main/java/com/demo/firstproject/models/CandidateXTechnology.java
+++ src/main/java/com/demo/firstproject/models/CandidateXTechnology.java
@@ -1,6 +1,8 @@
 package com.demo.firstproject.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.*;
 
@@ -12,15 +14,18 @@ import lombok.*;
 @Table (name="candidateXTechnology")
 @Entity
 public class CandidateXTechnology {
-    
+
+    @NotNull
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank (message = "El candidato no puede estar vacio")
     @ManyToOne
     @JoinColumn(name = "candidate", referencedColumnName = "id")
     private Candidate candidate;
 
+    @NotBlank(message =  "La tecnologia no puede estar vacia")
     @ManyToOne
     @JoinColumn(name = "technologyModel",referencedColumnName = "idTechnology")
     private Technology technology;
